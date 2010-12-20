5ae8bf3bb987d121bb58f2accc1d27fd3ebc5f0b
xspager <xspager@934b1e2d-1a5c-ebb4-d28c-15d990f817bb>
2010-12-20 02:13:51 +0000

    some fixes

--- libfb/src/demos/squares.c
+++ libfb/src/demos/squares.c
@@ -15,16 +15,16 @@ void main()
 	
 	srand(time(NULL));
 	
-	for(i=0; i<1000; i++){
-		x = (double) rand() * (15 + 1) / RAND_MAX;
-		p = (double) rand() * (100 + 1) / RAND_MAX;
-		x1 = x % 15 * (800 / 15);
-		x2 = x % 15 * (800 / 15);
-		y1 = x * (600 / 15);
-		y2 = x * (600 / 15);
-		lfb.fillbox(x1,y1,x2,y2,x);
-		printf("x = %i, x1 = %i, y1 = %i, x2 = %i, y2 = %i\n", x,x1,y1,x2,y2);
-		usleep(60000);
+	for(i=0; i < 1000; i++){
+		x = (double) rand() * 100 / RAND_MAX;
+		p = (double) rand() * 100 / RAND_MAX;
+		x1 = x % 100 * (lfb.width / 100);
+		x2 = p % 100 * (lfb.width / 100);
+		y1 = x * (lfb.height / 100);
+		y2 = p * (lfb.height / 100);
+		lfb.fillbox(x1,y1,x2,y2,x*p*x*p*x+128);
+		//printf("x = %i, x1 = %i, y1 = %i, x2 = %i, y2 = %i\n", x,x1,y1,x2,y2);
+		usleep(20000);
 	}
 }
 
